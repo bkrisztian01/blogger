effe08a26d391e534da05039fd30ee6fb3f5b009
bkrisztian01 <bui.krisztian01@gmail.com>
2019-11-02 13:35:06 +0100

    Added tag index

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,4 +2,13 @@ class TagsController < ApplicationController
 	def show
 		@tag = Tag.find(params[:id])
 	end
+
+	def destroy
+		@tag = Tag.find(params[:id])
+		@tag.destroy
+	end
+
+	def index
+		@tags = Tag.all
+	end
 end
